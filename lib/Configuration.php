96072900a38166ec7b1dfc64c770e27d24f5f036
Jenkins <integrations@e-goi.com>
2022-12-12 18:46:53 +0000

    Version:  1.1.4RC1

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -100,7 +100,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/1.1.3RC1/PHP';
+    protected $userAgent = 'OpenAPI-Generator/1.1.4RC1/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -433,7 +433,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 3.0.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 1.1.3RC1' . PHP_EOL;
+        $report .= '    SDK Package Version: 1.1.4RC1' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
